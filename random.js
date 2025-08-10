f918217d749c6f88371e09e3f34a64c5e9656686
bxfbf12 <culaalkan2020@gmail.com>
2025-08-10 23:36:30 +0330

    Create random.js

---
+++ random.js
@@ -0,0 +1,6 @@
+const fruits = ["🍎 Apple", "🍌 Banana", "🍇 Grape", "🍓 Strawberry", "🍊 Orange"];
+
+// Select a random item from the array
+const randomFruit = fruits[Math.floor(Math.random() * fruits.length)];
+
+console.log(randomFruit); // Example output: 🍇 Grape
